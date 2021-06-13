b10e953377dbbb69b68e6862383eeff5bd80d85a
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-13 19:01:41 -0400

    Rename offsets

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -22,33 +22,34 @@ class Enigma
     result = @offset.join.to_i
   end
 
-  def shift_a
-    ((@offset[0].to_i) + (@a_key.to_i))
+  def offset_a
+    @offset[0].to_i
   end
 
-  def shift_b
-    ((@offset[1].to_i) + (@b_key.to_i))
+  def offset_b
+    @offset[1].to_i
   end
 
-  def shift_c
-    ((@offset[2].to_i) + (@c_key.to_i))
+  def offset_c
+    @offset[2].to_i
   end
 
-  def shift_d
-    ((@offset[3].to_i) + (@d_key.to_i))
+  def offset_d
+    @offset[3].to_i
   end
 
   def encrypt(message, key = nil, date = nil)
+    message = message
     key ||= key_gen
     date ||= todays_date
-    @a_key = encrypt[:key][0,1]
-    @b_key = encrypt[:key][1,2]
-    @c_key = encrypt[:key][2,3]
-    @d_key = encrypt[:key][3,4]
-    require "pry"; binding.pry
-    {message: message,
+    { encryption: encrypted_message,
       key: key,
-      date: date}
+      date: date }
   end
+      #
+      # @a_key = ([:key]).to_i[0,1]
+      # @b_key = ([:key]).to_i[1,2]
+      # @c_key = ([:key]).to_i[2,3]
+      # @d_key = ([:key]).to_i[3,4]
 
 end
