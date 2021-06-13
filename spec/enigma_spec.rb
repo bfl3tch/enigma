090705ec62a8c2af04aa8bb24013f7e7170c4fba
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-13 11:52:54 -0400

    Create spec test for key_gen

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -8,4 +8,22 @@ RSpec.describe Enigma do
       expect(enigma).to be_an_instance_of(Enigma)
     end
   end
+
+  describe 'it has a method that can' do
+    it 'can generate a random key' do
+      enigma = Enigma.new
+
+      expect(enigma.key_gen).to be_a(String)
+      expect(enigma.key_gen.length).to eq(5)
+
+      allow(enigma).to receive(:key_gen).and_return("05555")
+      expect(enigma.key_gen).to eq("05555")
+    end
+    it 'can encrypt something' do
+      enigma = Enigma.new
+      expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+      expect(enigma.encrypt).to eq(expected)
+    end
+  end
 end
