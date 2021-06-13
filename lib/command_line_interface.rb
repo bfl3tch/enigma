923f9385ebecb722192073b1c3c313ae3734b0f2
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-13 17:15:47 -0400

    Remove class designation for CLI, adjust for rspec command

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,11 +1,14 @@
-class CommandLineInterface
+unencrypted_file = ARGV[0]
+unencrypted_file ||= './message.txt'
 
-handle = File.open(ARGV[0], "r")
+encrypted_file = ARGV[1]
+encrypted_file ||= './encrypted.txt'
+
+# encrypted_file = ARGV[1].nil? ? './encrypted.txt' : ARGV[1]
+
+handle = File.open(unencrypted_file, "r")
 incoming_text = handle.read
-encrypted_text = incoming_text.encrypt?????
-writer = file.open[ARGV[1], "w"]
+encrypted_text = incoming_text.upcase
+writer = File.open(encrypted_file, "w")
 writer.write(encrypted_text)
-wrter.close
-
-
-end
+writer.close
