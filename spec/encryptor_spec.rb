bfd5eef60b4e446f4d7102236c0e250e04bb198b
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-13 14:39:23 -0400

    Comment out encryptor tests, consider refactor

---
+++ spec/encryptor_spec.rb
@@ -0,0 +1,18 @@
+# require 'spec_helper'
+#
+# RSpec.describe Encryptor do
+#   before(:each) do
+#     @encryptor = Encryptor.new
+#   end
+#
+#   describe 'intantiation' do
+#     it 'creates a new object' do
+#       expect(@encryptor).to be_an_instance_of(Encryptor)
+#     end
+#   end
+#
+#   describe 'it has a method that' do
+#
+#     end
+#   end
+# end
