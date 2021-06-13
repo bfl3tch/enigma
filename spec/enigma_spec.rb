ddc441939988da19f9973dab03e8d5440e605f4b
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-13 18:09:05 -0400

    Create tests for key generation

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -26,11 +26,11 @@ RSpec.describe Enigma do
 
     it 'can calculate the shifts' do
       @enigma.offset("040621")
+
       expect(@enigma.shift_a).to eq(5)
       expect(@enigma.shift_b).to eq(6)
       expect(@enigma.shift_c).to eq(4)
       expect(@enigma.shift_d).to eq(1)
-
     end
 
     it 'can encrypt something' do
@@ -44,6 +44,10 @@ RSpec.describe Enigma do
       expect(test[:encryption]).to eq(expected[:encryption])
       expect(test[:key]).to eq(expected[:key])
       expect(test[:date]).to eq(expected[:date])
+      expect(@a_key).to eq(21)
+      expect(@b_key).to eq(13)
+      expect(@c_key).to eq(34)
+      expect(@d_key).to eq(45)
     end
 
 
