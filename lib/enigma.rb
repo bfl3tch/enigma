a5db7129a11d807aed698f2169c34489b5db9e14
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-13 18:09:27 -0400

    Adjust key locations

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -23,30 +23,32 @@ class Enigma
   end
 
   def shift_a
-    @offset[0].to_i
+    ((@offset[0].to_i) + (@a_key.to_i))
   end
 
   def shift_b
-    @offset[1].to_i
+    ((@offset[1].to_i) + (@b_key.to_i))
   end
 
   def shift_c
-    @offset[2].to_i
+    ((@offset[2].to_i) + (@c_key.to_i))
   end
 
   def shift_d
-    @offset[3].to_i
+    ((@offset[3].to_i) + (@d_key.to_i))
   end
 
   def encrypt(message, key = nil, date = nil)
     key ||= key_gen
     date ||= todays_date
-    # or ^ operator, looks for t/f.  if false, set to right side, if true
-    # move on
+    @a_key = encrypt[:key][0,1]
+    @b_key = encrypt[:key][1,2]
+    @c_key = encrypt[:key][2,3]
+    @d_key = encrypt[:key][3,4]
+    require "pry"; binding.pry
     {message: message,
-    key: key_gen,
-    date: date}
-    offset(date)
+      key: key,
+      date: date}
   end
 
 end
