d14b4279b36de0202089dc0ee43ee50559f3398a
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-13 11:53:17 -0400

    Create key_gen method and date method for encrypt defaults

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,20 @@
 class Enigma
 
+  def initialize
+
+  end
+
+  def key_gen
+    rand(99999).to_s.rjust(5, "0")
+  end
+
+  def todays_date
+     Date.today.strftime("%m%d%y")
+  end
+
+  def encrypt(message, key = key_gen, date = todays_date)
+    alphabet.create_alphabet
+  end
+
 
 end
