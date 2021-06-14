a2cb6c7e659f762865bb6bffdcab0dbd30e24abe
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 13:49:33 -0400

    Create new decryptor class

---
+++ lib/decryptor.rb
@@ -0,0 +1,40 @@
+class Decryptor
+  include Alphabet
+  #make sure to test all module methods
+  attr_reader :key, :date, :offset
+
+  def initialize(key, date)
+    @key = key
+    @date = date
+    @offset = Offset.new(date)
+  end
+
+  def final_offset
+    @offset.full_offset
+  end
+
+  def offset_split
+    @offset.last_four_as_integers
+  end
+
+  def keys_as_ints
+    split_key = @key.split("")
+    key_split_integer = split_key.map { |key| key.to_i }
+  end
+
+  def split_keys_into_pairs
+    keys_as_ints.join.chars.each_cons(2).map { |pair| pair.join.to_i }
+  end
+
+  def keys_plus_offsets
+    offset_split.map.with_index do |number, index|
+      number + split_keys_into_pairs[index]
+    end
+  end
+
+  def full_shift
+    keys_plus_offsets.map do |number|
+      number % 27
+    end
+  end
+end
