9e084aad613d8f5d8231bbf861418614397f46b1
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 00:39:34 -0400

    Shift tests and update

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -8,4 +8,29 @@ RSpec.describe Key do
     expect(key).to be_an_instance_of(Key)
     end
   end
+
+  describe 'it has a method that can' do
+    it 'can generate a random key' do
+      expect(@enigma.key_gen).to be_a(String)
+      expect(@enigma.key_gen.length).to eq(5)
+
+      allow(@enigma).to receive(:key_gen).and_return("05555")
+      expect(@enigma.key_gen).to eq("05555")
+    end
+
+    it 'can calculate the key' do
+      @enigma.encrypt("hello world", "02715", "040895")
+
+      expect(@enigma.keys).to eq(["0", "2", "7", "1", "5"])
+    end
+
+    it 'can calculate the individual keys' do
+      @enigma.encrypt("hello world", "02715", "040895")
+
+      expect(@enigma.a_key).to eq(2)
+      expect(@enigma.b_key).to eq(27)
+      expect(@enigma.c_key).to eq(71)
+      expect(@enigma.d_key).to eq(15)
+    end
+  end
 end
