baa1b4785127f54320d61bebace1fbed0528eee8
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 00:39:37 -0400

    Shift tests and update

--- spec/offsets_spec.rb
+++ spec/offsets_spec.rb
@@ -1,11 +1,34 @@
 require 'spec_helper'
 
 RSpec.describe Offset do
+  before(:each) do
+    @offset = Offset.new
+  end
+
   describe 'instantiation' do
     it 'creates an offset' do
-    offset = Offset.new
+    expect(@offset).to be_an_instance_of(Offset)
+    end
 
-    expect(offset).to be_an_instance_of(Offset)
+    it 'has readable attributes' do
+      expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+      expect(@offset.alphabet).to eq(expected)
+
+    end
+  end
+  describe 'it has a method that' do
+    it 'can calculate the offset' do
+      expect(@enigma.offset("040621")).to eq(5641)
+    end
+
+    it 'can calculate the individual offsets' do
+      @enigma.offset("040621")
+
+      expect(@enigma.offset_a).to eq(5)
+      expect(@enigma.offset_b).to eq(6)
+      expect(@enigma.offset_c).to eq(4)
+      expect(@enigma.offset_d).to eq(1)
     end
   end
 end
