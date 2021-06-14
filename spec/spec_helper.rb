3e316e565669b61cfde5e79811c62eb65c35836c
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 00:42:04 -0400

    Update file name

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require './lib/enigma'
 require './lib/key'
-require './lib/offsets'
+require './lib/offset'
 require './lib/command_line_interface'
 require './lib/encryptor'
 require 'simplecov'
