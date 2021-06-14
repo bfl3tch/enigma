5e704b35b183e8ba361e435a71270f830f531012
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 11:46:20 -0400

    Match formatting for decrypt runner - need to update for reverse

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,1 +1,16 @@
-.
+require './enigma'
+require './key'
+require './offset'
+require './encryptor'
+require './decryptor'
+require './command_line_interface'
+
+#FIX FOR DECRYPTION - THIS IS ENCRYPTION
+enigma = Enigma.new
+handle = File.open(ARGV[0], "r")
+incoming_text = handle.read.downcase
+encrypted_text = incoming_text.enigma.encrypt
+
+writer = File.open(ARGV[1], "w")
+writer.write(encrypted_text)
+writer.close
