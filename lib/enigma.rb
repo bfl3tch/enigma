6f8e9901ac7ce3b4cdfd6ff4e334f36b6e78562d
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 11:41:20 -0400

    Setup for encrypt method build

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,29 +5,29 @@ class Enigma
   def initialize
   end
 
-  def a_shift
-    (offset_a + a_key) % 27
-  end
-
-  def b_shift
-    (offset_b + b_key) % 27
-  end
-
-  def c_shift
-    (offset_c + c_key) % 27
-  end
-
-  def d_shift
-    (offset_d + d_key) % 27
-  end
-
-  def full_shift
-
-  end
+  # def a_shift
+  #   (offset_a + a_key) % 27
+  # end
+  #
+  # def b_shift
+  #   (offset_b + b_key) % 27
+  # end
+  #
+  # def c_shift
+  #   (offset_c + c_key) % 27
+  # end
+  #
+  # def d_shift
+  #   (offset_d + d_key) % 27
+  # end
+  #
+  # def full_shift
+  #
+  # end
 
   def encrypt(message, key = nil, date = nil)
     key ||= key.key_gen
-    date ||= todays_date
+    date ||= offset.todays_date
     offset = Offset.new(date)
     @message = message
     @cryptic =
@@ -36,18 +36,11 @@ class Enigma
       date: date }
   end
 
-
-
-
   def encrypted_message
-    alphabet = alphabet_with_indexes
+    alphabet =  create_alphabet
+    alphab_with_i = alphabet_with_indexes
     ready_to_shift = @message.split("")
-    #apply a shift to each 1st, 5th, 9th etc chars etc
-      #how to assign individual shifts?
-    #apply b shift to each 2nd, 6th, 10th etc chars etc
-    #apply c shift to each 3rd, 7th, 11th etc chars etc
-    #apply d shift to each 4th, 8th, 12th etc chars etc
-    # alphabet = alphabet_with_indexes
+    # require "pry"; binding.pry
     shifted = []
     ready_to_shift.map do |letter|
 
