3eb267238b4542afbd7710951f704bc4c4617ad6
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 13:31:37 -0400

    Create spec tests for helper methods and encrypted text

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -31,17 +31,11 @@ RSpec.describe Enigma do
       expect(@enigma.message_broken_in_fours_integers).to eq(expected)
 
     end
-    # it 'can break the incoming integer-converted message into fours' do
-    #   @enigma.encrypt("hello world", "02715", "040895")
-    #   expected = [[7, 4, 11, 11], [14, 26, 22, 14], [17, 11, 3]]
-    #   expect(@enigma.message_broken_in_fours).to eq(expected)
-    #
-    # end
 
-    it 'can translate the message into numbers' do
+    it 'can translate the message into encrypted text' do
       @enigma.encrypt("hello world", "02715", "040895")
-      expected = [[8, 4, 12, 12], ["o", " ", "w", "o"], ["r", "l", "d"]]
-      expect(@enigma.fours_as_integers).to eq(expected)
+      expected = ["k", "e", "d", "e", "r", " ", "o", "h", "u", "l", "w"]
+      expect(@enigma.shifted_fours).to eq(expected)
 
     end
     it 'can encrypt something' do
@@ -55,10 +49,6 @@ RSpec.describe Enigma do
       expect(test[:encryption]).to eq(expected[:encryption])
       expect(test[:key]).to eq(expected[:key])
       expect(test[:date]).to eq(expected[:date])
-      expect(@a_key).to eq(21)
-      expect(@b_key).to eq(13)
-      expect(@c_key).to eq(34)
-      expect(@d_key).to eq(45)
     end
   end
 end
