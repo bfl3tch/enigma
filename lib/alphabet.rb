d33fe0621d72901b05ee00c53c30b45443ae23ba
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 01:17:14 -0400

    Alphabet as included mixin

--- lib/alphabet.rb
+++ lib/alphabet.rb
@@ -1,10 +1,10 @@
 module Alphabet
 
-  def self.create_alphabet
+  def create_alphabet
     ("a".."z").to_a << " "
   end
 
-  def self.alphabet_with_indexes
+  def alphabet_with_indexes
     create_alphabet.zip((0..27).to_a)
   end
 
