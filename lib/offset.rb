823de10b28607e941aba8e62ca6e5fd40cba036a
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 02:02:58 -0400

    Adjust offset for iteration

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,6 +1,5 @@
 require 'alphabet'
 require 'enigma'
-require 'date'
 
 class Offset
   include Alphabet
@@ -23,33 +22,34 @@ class Offset
     square_date.to_s.split("")
   end
 
-  def full_offset
-    @offset = split_date[-4, 4]
-    full_offset = @offset.join.to_i
+  def last_four_split
+    split_date[-4, 4]
   end
 
-  # def todays_date
-  #    Date.today.strftime("%d%m%y")
+  def full_offset
+    last_four_split.join.to_i
+  end
+
+  # def offset_assignment
+  #   full_offset.each do |offset|
+  #     require "pry"; binding.pry
+  #   end
+  # 
   # end
 
-  def today_date_as_integer
-    todays_date.to_i
-  end
-
-  def offset_a
-    @offset[0].to_i
-  end
-
-  def offset_b
-    @offset[1].to_i
-  end
-
-  def offset_c
-    @offset[2].to_i
-  end
-
-  def offset_d
-    @offset[3].to_i
-  end
-
+  # def offset_a
+  #   @offset[0].to_i
+  # end
+  #
+  # def offset_b
+  #   @offset[1].to_i
+  # end
+  #
+  # def offset_c
+  #   @offset[2].to_i
+  # end
+  #
+  # def offset_d
+  #   @offset[3].to_i
+  # end
 end
