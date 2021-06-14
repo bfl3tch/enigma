91f07114e7f00fc111ce8fd183497d0856137893
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 09:29:10 -0400

    Update attr_reader to pass instantiation test

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,7 +1,7 @@
 class Encryptor
   include Alphabet
   #make sure to test all module methods
-  attr_reader :key, :date
+  attr_reader :key, :date, :offset
 
   def initialize(key, date)
     @key = key
@@ -10,10 +10,14 @@ class Encryptor
   end
 
   def full_shift
-    good_offset = @offset.full_offset
-    good_key = @key
-    require "pry"; binding.pry
-
+    final_offset = @offset.full_offset
+    split_key = @key.split("")
+    shifty = split_key.map_with_index do |number, index|
+      number + final_offset[index]
+    end
+    shifty.map do |num|
+      num % 27
+    end
   end
 
 
