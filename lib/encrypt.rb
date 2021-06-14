89fac73d26414ac8edd866249aa886dd599e7e14
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 15:06:09 -0400

    Structure encrypt

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,30 +1,21 @@
-require_relative 'enigma'
 require_relative 'alphabet'
+require_relative 'decryptor'
+require_relative 'encryptor'
+require_relative 'enigma'
 require_relative 'key'
 require_relative 'offset'
-require_relative 'encryptor'
-require_relative 'decryptor'
-require_relative 'command_line_interface'
 
-#
-# enigma = Enigma.new
-# handle = File.open(ARGV[0], "r")
-# incoming_text = handle.read.downcase
-# encrypted_text = incoming_text.enigma.encrypt
-#
-# writer = File.open(ARGV[1], "w")
-# writer.write(encrypted_text)
-# writer.close
 unencrypted_file = ARGV[0]
 unencrypted_file ||= './message.txt'
 encrypted_file = ARGV[1]
 encrypted_file ||= './encrypted.txt'
 
-# encrypted_file = ARGV[1].nil? ? './encrypted.txt' : ARGV[1]
-enigma = Enigma.new
 handle = File.open(unencrypted_file, "r")
+@enigma = Enigma.new
+# key = key.key_gen
+# date = offset.date
 incoming_text = handle.read.downcase
-encrypted_text = enigma.encrypt(incoming_text)
+encrypted_text = @enigma.encrypt(incoming_text)
 writer = File.open(encrypted_file, "w")
 writer.write(encrypted_text)
 writer.close
