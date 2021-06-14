99fc7fd062976f32c75437b961d98b51e74ae9cf
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 02:03:32 -0400

    Update key tests

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -1,36 +1,38 @@
 require 'spec_helper'
 
 RSpec.describe Key do
+  before(:each) do
+    @key = Key.new
+  end
+  
   describe 'instantiation' do
     it 'creates a key' do
-    key = Key.new
-
     expect(key).to be_an_instance_of(Key)
     end
   end
 
   describe 'it has a method that can' do
     it 'can generate a random key' do
-      expect(@enigma.key_gen).to be_a(String)
-      expect(@enigma.key_gen.length).to eq(5)
+      expect(@key.key_gen).to be_a(String)
+      expect(@key.key_gen.length).to eq(5)
 
-      allow(@enigma).to receive(:key_gen).and_return("05555")
-      expect(@enigma.key_gen).to eq("05555")
+      allow(@key).to receive(:key_gen).and_return("05555")
+      expect(@key.key_gen).to eq("05555")
     end
 
     it 'can calculate the key' do
-      @enigma.encrypt("hello world", "02715", "040895")
+      @key.encrypt("hello world", "02715", "040895")
 
-      expect(@enigma.keys).to eq(["0", "2", "7", "1", "5"])
+      expect(@key.keys).to eq(["0", "2", "7", "1", "5"])
     end
 
     it 'can calculate the individual keys' do
-      @enigma.encrypt("hello world", "02715", "040895")
+      @key.encrypt("hello world", "02715", "040895")
 
-      expect(@enigma.a_key).to eq(2)
-      expect(@enigma.b_key).to eq(27)
-      expect(@enigma.c_key).to eq(71)
-      expect(@enigma.d_key).to eq(15)
+      expect(@key.a_key).to eq(2)
+      expect(@key.b_key).to eq(27)
+      expect(@key.c_key).to eq(71)
+      expect(@key.d_key).to eq(15)
     end
   end
 end
