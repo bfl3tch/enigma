e435f48eb15c915492c9b603e58ba91a1c5846dc
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 16:47:17 -0400

    Reconfigure SimpleCov for all classes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
 require 'rspec'
+require 'simplecov'
+SimpleCov.start
+SimpleCov.add_filter ['spec']
 require './lib/enigma'
 require './lib/key'
 require './lib/offset'
 require './lib/encryptor'
 require './lib/decryptor'
 require './lib/alphabet'
-require 'simplecov'
-SimpleCov.start
