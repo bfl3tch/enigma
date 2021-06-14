8bbc786ee5a27ae1af0b7334cfcf322072142aef
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 00:00:03 -0400

    Moved all key and offset items to respective class files

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,17 +6,8 @@ class Enigma
 
   end
 
-  def todays_date
-     Date.today.strftime("%d%m%y")
-  end
 
-  def today_date_as_integer
-    todays_date.to_i
-  end
 
-  def key_gen
-    rand(99999).to_s.rjust(5, "0")
-  end
 
   def encrypt(message, key = nil, date = nil)
     key ||= key_gen
@@ -27,47 +18,7 @@ class Enigma
       date: date }
   end
 
-  def offset(date = @hash[:date])
-    string_split = (date.to_i ** 2).to_s.split("")
-    @offset = string_split[-4, 4]
-    full_offset = @offset.join.to_i
-  end
 
-  def offset_a
-    @offset[0].to_i
-  end
-
-  def offset_b
-    @offset[1].to_i
-  end
-
-  def offset_c
-    @offset[2].to_i
-  end
-
-  def offset_d
-    @offset[3].to_i
-  end
-
-  def keys
-    @hash[:key].split("")
-  end
-
-  def a_key
-    "#{keys[0]}#{keys[1]}".chomp.to_i
-  end
-
-  def b_key
-    "#{keys[1]}#{keys[2]}".chomp.to_i
-  end
-
-  def c_key
-    "#{keys[2]}#{keys[3]}".chomp.to_i
-  end
-
-  def d_key
-    "#{keys[3]}#{keys[4]}".chomp.to_i
-  end
 
   def a_shift
     (offset_a + a_key) % 27
