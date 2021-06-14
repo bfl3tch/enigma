9fe84d1bd9dd056c2779baf89e9f91f7756fe743
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 00:39:39 -0400

    Shift tests and update

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -10,43 +10,8 @@ RSpec.describe Enigma do
       expect(@enigma).to be_an_instance_of(Enigma)
     end
   end
-
+  
   describe 'it has a method that can' do
-    it 'can generate a random key' do
-      expect(@enigma.key_gen).to be_a(String)
-      expect(@enigma.key_gen.length).to eq(5)
-
-      allow(@enigma).to receive(:key_gen).and_return("05555")
-      expect(@enigma.key_gen).to eq("05555")
-    end
-
-    it 'can calculate the offset' do
-      expect(@enigma.offset("040621")).to eq(5641)
-    end
-
-    it 'can calculate the individual offsets' do
-      @enigma.offset("040621")
-
-      expect(@enigma.offset_a).to eq(5)
-      expect(@enigma.offset_b).to eq(6)
-      expect(@enigma.offset_c).to eq(4)
-      expect(@enigma.offset_d).to eq(1)
-    end
-
-    it 'can calculate the key' do
-      @enigma.encrypt("hello world", "02715", "040895")
-
-      expect(@enigma.keys).to eq(["0", "2", "7", "1", "5"])
-    end
-    it 'can calculate the individual keys' do
-      @enigma.encrypt("hello world", "02715", "040895")
-
-      expect(@enigma.a_key).to eq(2)
-      expect(@enigma.b_key).to eq(27)
-      expect(@enigma.c_key).to eq(71)
-      expect(@enigma.d_key).to eq(15)
-    end
-
     it 'can generate 4 individual shifts' do
       @enigma.offset("040621")
       @enigma.encrypt("hello world", "02715", "040621")
