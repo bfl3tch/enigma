b9de6ae03cbffeaa20064b8dbc0096b65222b36c
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 11:40:59 -0400

    Remove unnecessary test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -12,23 +12,6 @@ RSpec.describe Enigma do
   end
 
   describe 'it has a method that can' do
-    # it 'can generate 4 individual shifts' do
-    #   @enigma.offset("040621")
-    #   @enigma.encrypt("hello world", "02715", "040621")
-    #
-    #   expect(@enigma.a_shift).to eq(7)
-    #   expect(@enigma.b_shift).to eq(6)
-    #   expect(@enigma.c_shift).to eq(21)
-    #   expect(@enigma.d_shift).to eq(16)
-    #
-    # end
-
-    # it 'can calculate the key' do
-    #   @key.encrypt("hello world", "02715", "040895")
-    #
-    #   expect(@key.keys).to eq(["0", "2", "7", "1", "5"])
-    # end
-
     it 'can encrypt something' do
       test = @enigma.encrypt("hello world", "02715", "040895")
       expected = {
