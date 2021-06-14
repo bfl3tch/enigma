2da1a5fce47b2e5c1ab594443c5e01094532f4c4
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 15:05:36 -0400

    Update spec_helper to lose CLI

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,16 +1,30 @@
-require './enigma'
-require './key'
-require './offset'
-require './encryptor'
-require './decryptor'
-require './command_line_interface'
+require_relative 'enigma'
+require_relative 'alphabet'
+require_relative 'key'
+require_relative 'offset'
+require_relative 'encryptor'
+require_relative 'decryptor'
+require_relative 'command_line_interface'
 
+#
+# enigma = Enigma.new
+# handle = File.open(ARGV[0], "r")
+# incoming_text = handle.read.downcase
+# encrypted_text = incoming_text.enigma.encrypt
+#
+# writer = File.open(ARGV[1], "w")
+# writer.write(encrypted_text)
+# writer.close
+unencrypted_file = ARGV[0]
+unencrypted_file ||= './message.txt'
+encrypted_file = ARGV[1]
+encrypted_file ||= './encrypted.txt'
 
+# encrypted_file = ARGV[1].nil? ? './encrypted.txt' : ARGV[1]
 enigma = Enigma.new
-handle = File.open(ARGV[0], "r")
+handle = File.open(unencrypted_file, "r")
 incoming_text = handle.read.downcase
-encrypted_text = incoming_text.enigma.encrypt
-
-writer = File.open(ARGV[1], "w")
+encrypted_text = enigma.encrypt(incoming_text)
+writer = File.open(encrypted_file, "w")
 writer.write(encrypted_text)
 writer.close
