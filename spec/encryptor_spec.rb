abc5aac210663dbbcae8c1bd6028dcfc07654296
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 10:01:58 -0400

    Create tests for helper methods

--- spec/encryptor_spec.rb
+++ spec/encryptor_spec.rb
@@ -18,6 +18,19 @@ RSpec.describe Encryptor do
   end
 
   describe 'it has a method that' do
+    it 'can determine the final offset' do
+    expect(@encryptor.final_offset).to eq(1025)
+    end
+
+    it 'can determine the final keys as array of integers' do
+    expect(@encryptor.keys_as_ints).to eq([0, 2, 7, 1, 5])
+    end
+
+    it 'can split the keys into the required pairs' do
+      expect(@encryptor.split_keys_into_pairs).to eq([0, 2, 7, 1, 5])
+
+    end
+
     it 'can determine the full shift' do
       expect(@encryptor.full_shift).to eq(3512)
 
