c9e6f6070301a9e1bab240ee2117f2ac478b5f59
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 10:29:13 -0400

    Create tests for necessary data collection

--- spec/encryptor_spec.rb
+++ spec/encryptor_spec.rb
@@ -26,15 +26,21 @@ RSpec.describe Encryptor do
     expect(@encryptor.keys_as_ints).to eq([0, 2, 7, 1, 5])
     end
 
-    it 'can split the keys into the required pairs' do
-      expect(@encryptor.split_keys_into_pairs).to eq([0, 2, 7, 1, 5])
+    it 'can split the full offset' do
+      expect(@encryptor.offset_split).to eq([1, 0, 2, 5])
 
     end
 
+    it 'can split the keys into the required pairs' do
+      expect(@encryptor.split_keys_into_pairs).to eq([2, 27, 71, 15])
+    end
+
+    it 'can add the keys and the offsets' do
+      expect(@encryptor.keys_plus_offsets).to eq([3, 27, 73, 20])
+    end
+
     it 'can determine the full shift' do
-      expect(@encryptor.full_shift).to eq(3512)
-
-
+      expect(@encryptor.full_shift).to eq([3, 0, 19, 20])
     end
   end
 end
