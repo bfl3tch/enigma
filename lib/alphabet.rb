b0e72d05d324c228613004b41b0236b3fa51b745
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 20:15:18 -0400

    Comment out unused method for alphabet with index positions

--- lib/alphabet.rb
+++ lib/alphabet.rb
@@ -3,9 +3,9 @@ module Alphabet
   def create_alphabet
     ("a".."z").to_a << " "
   end
-
-  def alphabet_with_indexes
-    create_alphabet.zip((0..27).to_a)
-  end
+  # 
+  # def alphabet_with_indexes
+  #   create_alphabet.zip((0..27).to_a)
+  # end
 
 end
