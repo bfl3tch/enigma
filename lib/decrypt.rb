fd6903050b66855b8d88d784476fb0ced336fe3e
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-14 23:41:03 -0400

    Finalized formatting for decrypt runner

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -10,11 +10,17 @@ encrypted_file ||= './encrypted.txt'
 decrypted_file = ARGV[1]
 decrypted_file ||= './decrypted.txt'
 
+key = Key.new(ARGV[2])
+date = Offset.new(ARGV[3])
+
 handle = File.open(encrypted_file, "r")
 @enigma = Enigma.new
-incoming_text = handle.read.downcase
 
-encrypted_text = @enigma.decrypt(incoming_text)
+incoming_text = handle.read.downcase
+decrypt = Decryptor.new(incoming_text, key, date)
+decrypted_text = @enigma.decrypt(incoming_text, key, date)
 writer = File.open(decrypted_file, "w")
 writer.write(decrypted_file)
 writer.close
+
+puts "Created '#{decrypted_file}' with the key #{@enigma.decrypted_output[:key].key} and date #{@enigma.decrypted_output[:date].date}"
